c0617ae765776a57854f88540125f301aa9a1a4d
Travis Carden <travis.carden@gmail.com>
2021-05-10 16:19:45 -0400

    Add Cleaner.

---
+++ src/Domain/Cleaner.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace PhpTuf\ComposerStager\Domain;
+
+use PhpTuf\ComposerStager\Exception\DirectoryNotFoundException;
+use PhpTuf\ComposerStager\Infrastructure\Filesystem\Filesystem;
+
+class Cleaner
+{
+    /**
+     * @var \PhpTuf\ComposerStager\Infrastructure\Filesystem\Filesystem
+     */
+    private $filesystem;
+
+    public function __construct(Filesystem $filesystem)
+    {
+        $this->filesystem = $filesystem;
+    }
+
+    /**
+     * @throws \PhpTuf\ComposerStager\Exception\DirectoryNotFoundException
+     */
+    public function clean(string $stagingDir): void
+    {
+        if (!$this->directoryExists($stagingDir)) {
+            throw new DirectoryNotFoundException($stagingDir, 'The staging directory does not exist at "%s"');
+        }
+
+        $this->filesystem->remove($stagingDir);
+    }
+
+    public function directoryExists(string $stagingDir): bool
+    {
+        return $this->filesystem->exists($stagingDir);
+    }
+}
