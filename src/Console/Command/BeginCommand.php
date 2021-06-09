ecc0c63ac6782292c18071e5e527feaa05cba1db
Travis Carden <travis.carden@gmail.com>
2021-06-09 10:15:54 -0400

    Implement "begin" command.

--- src/Console/Command/BeginCommand.php
+++ src/Console/Command/BeginCommand.php
@@ -2,7 +2,11 @@
 
 namespace PhpTuf\ComposerStager\Console\Command;
 
+use PhpTuf\ComposerStager\Console\Application;
 use PhpTuf\ComposerStager\Console\Misc\ExitCode;
+use PhpTuf\ComposerStager\Console\Output\ProcessCallback;
+use PhpTuf\ComposerStager\Domain\BeginnerInterface;
+use PhpTuf\ComposerStager\Exception\ExceptionInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -14,9 +18,15 @@ final class BeginCommand extends Command
 {
     private const NAME = 'begin';
 
-    public function __construct()
+    /**
+     * @var \PhpTuf\ComposerStager\Domain\BeginnerInterface
+     */
+    private $beginner;
+
+    public function __construct(BeginnerInterface $beginner)
     {
         parent::__construct(self::NAME);
+        $this->beginner = $beginner;
     }
 
     protected function configure(): void
@@ -24,8 +34,37 @@ final class BeginCommand extends Command
         $this->setDescription('Begins the staging process by copying the active directory to the staging directory');
     }
 
+    /**
+     * @throws \Symfony\Component\Console\Exception\InvalidArgumentException
+     */
     public function execute(InputInterface $input, OutputInterface $output): int
     {
-        return ExitCode::SUCCESS;
+        /** @var string $activeDir */
+        $activeDir = $input->getOption(Application::ACTIVE_DIR_OPTION);
+        /** @var string $stagingDir */
+        $stagingDir = $input->getOption(Application::STAGING_DIR_OPTION);
+
+        if (!$this->beginner->activeDirectoryExists($activeDir)) {
+            $output->writeln(sprintf('<error>The active directory does not exist at "%s"</error>', $activeDir));
+            return ExitCode::FAILURE;
+        }
+
+        if ($this->beginner->stagingDirectoryExists($stagingDir)) {
+            $output->writeln(sprintf('<error>The staging directory already exists at "%s"</error>', $stagingDir));
+            return ExitCode::FAILURE;
+        }
+
+        try {
+            $this->beginner->begin(
+                $activeDir,
+                $stagingDir,
+                new ProcessCallback($input, $output)
+            );
+
+            return ExitCode::SUCCESS;
+        } catch (ExceptionInterface $e) {
+            $output->writeln("<error>{$e->getMessage()}</error>");
+            return ExitCode::FAILURE;
+        }
     }
 }
--- tests/Console/Command/BeginCommandTest.php
+++ tests/Console/Command/BeginCommandTest.php
@@ -2,9 +2,14 @@
 
 namespace PhpTuf\ComposerStager\Tests\Console\Command;
 
+use PhpTuf\ComposerStager\Console\Application;
 use PhpTuf\ComposerStager\Console\Command\BeginCommand;
 use PhpTuf\ComposerStager\Console\Misc\ExitCode;
+use PhpTuf\ComposerStager\Domain\BeginnerInterface;
+use PhpTuf\ComposerStager\Exception\IOException;
+use PhpTuf\ComposerStager\Exception\ProcessFailedException;
 use PhpTuf\ComposerStager\Tests\Console\CommandTestCase;
+use Prophecy\Argument;
 use Symfony\Component\Console\Command\Command;
 
 /**
@@ -12,12 +17,30 @@ use Symfony\Component\Console\Command\Command;
  * @covers ::__construct
  * @uses \PhpTuf\ComposerStager\Console\Application
  * @uses \PhpTuf\ComposerStager\Console\Command\BeginCommand
+ * @uses \PhpTuf\ComposerStager\Console\Output\ProcessCallback
+ *
+ * @property \PhpTuf\ComposerStager\Domain\BeginnerInterface|\Prophecy\Prophecy\ObjectProphecy beginner
  */
 class BeginCommandTest extends CommandTestCase
 {
+    protected function setUp(): void
+    {
+        $this->beginner = $this->prophesize(BeginnerInterface::class);
+        $this->beginner
+            ->activeDirectoryExists(Argument::any())
+            ->willReturn(true);
+        $this->beginner
+            ->stagingDirectoryExists(Argument::any())
+            ->willReturn(false);
+        $this->beginner
+            ->begin(Argument::cetera());
+        parent::setUp();
+    }
+
     protected function createSut(): Command
     {
-        return new BeginCommand();
+        $beginner = $this->beginner->reveal();
+        return new BeginCommand($beginner);
     }
 
     /**
@@ -47,5 +70,74 @@ class BeginCommandTest extends CommandTestCase
 
         self::assertSame('', $this->getDisplay(), 'Displayed correct output.');
         self::assertSame(ExitCode::SUCCESS, $this->getStatusCode(), 'Returned correct status code.');
+    }
+
+    /**
+     * @covers ::execute
+     */
+    public function testActiveDirectoryNotFound(): void
+    {
+        $this->beginner
+            ->activeDirectoryExists(Argument::any())
+            ->willReturn(false);
+        $this->beginner
+            ->begin(Argument::cetera())
+            ->shouldNotBeCalled();
+
+        $this->executeCommand([]);
+
+        $expectedDisplay = sprintf(
+            '/active directory.*not.*"%s"/',
+            preg_quote(Application::DEFAULT_ACTIVE_DIR, '\'')
+        );
+        self::assertMatchesRegularExpression($expectedDisplay, $this->getDisplay(), 'Displayed correct output.');
+        self::assertSame(ExitCode::FAILURE, $this->getStatusCode(), 'Returned correct status code.');
+    }
+
+    /**
+     * @covers ::execute
+     */
+    public function testStagingDirectoryAlreadyExists(): void
+    {
+        $this->beginner
+            ->stagingDirectoryExists(Argument::any())
+            ->willReturn(true);
+        $this->beginner
+            ->begin(Argument::cetera())
+            ->shouldNotBeCalled();
+
+        $this->executeCommand([]);
+
+        $expectedDisplay = sprintf(
+            '/staging directory.*exists.*"%s"/',
+            preg_quote(Application::DEFAULT_STAGING_DIR, '\'')
+        );
+        self::assertMatchesRegularExpression($expectedDisplay, $this->getDisplay(), 'Displayed correct output.');
+        self::assertSame(ExitCode::FAILURE, $this->getStatusCode(), 'Returned correct status code.');
+    }
+
+    /**
+     * @covers ::execute
+     *
+     * @dataProvider providerCommandFailure
+     */
+    public function testCommandFailure($exception, $message): void
+    {
+        $this->beginner
+            ->begin(Argument::cetera())
+            ->willThrow($exception);
+
+        $this->executeCommand([]);
+
+        self::assertSame($message . PHP_EOL, $this->getDisplay(), 'Displayed correct output.');
+        self::assertSame(ExitCode::FAILURE, $this->getStatusCode(), 'Returned correct status code.');
+    }
+
+    public function providerCommandFailure(): array
+    {
+        return [
+            ['exception' => new IOException('Lorem'), 'message' => 'Lorem'],
+            ['exception' => new ProcessFailedException('Ipsum'), 'message' => 'Ipsum'],
+        ];
     }
 }
