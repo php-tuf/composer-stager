b5973515ce8533869e041c00289f2dbcdd266cd7
Travis Carden <travis.carden@gmail.com>
2021-06-18 13:55:46 -0400

    Add functional test framework.

--- tests/Functional/TestCase.php
+++ tests/Functional/TestCase.php
@@ -4,4 +4,20 @@ namespace PhpTuf\ComposerStager\Tests\Functional;
 
 abstract class TestCase extends \PHPUnit\Framework\TestCase
 {
+    private $testEnv;
+
+    protected function setUp(): void
+    {
+        // Create test environment,
+        $this->testEnv = realpath(__DIR__ . '/../../var/test_env/') . md5(mt_rand());
+        mkdir($this->testEnv);
+    }
+
+    protected function tearDown(): void
+    {
+        // Remove test environment.
+        if (file_exists($this->testEnv)) {
+            rmdir($this->testEnv);
+        }
+    }
 }
