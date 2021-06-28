3858af2608beb78dfe518ae71f27365cc3fd2b00
Travis Carden <travis.carden@gmail.com>
2021-06-28 10:17:47 -0400

    Fix code comment. [skip ci]

--- src/Console/Command/AbstractCommand.php
+++ src/Console/Command/AbstractCommand.php
@@ -11,7 +11,7 @@ abstract class AbstractCommand extends Command
 {
     // sysexits-compatible exit codes.
     // See https://tldp.org/LDP/abs/html/exitcodes.html
-    // @todo As of 5.2 these are defined in \Symfony\Component\Console\Command\Command.
+    // @todo As of Symfony 5.2 these are defined in \Symfony\Component\Console\Command\Command.
     //   Remove them once we drop support for Symfony 4.x.
     public const SUCCESS = 0;
     public const FAILURE = 1;
