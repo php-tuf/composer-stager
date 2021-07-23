587b187128e48820b3ec1d66b71d341bf4fd7031
Travis Carden <travis.carden@gmail.com>
2021-07-23 09:31:51 -0400

    Reorganize PHPStan rules.

--- tests/PHPStan/Classes/ConcreteClassNotFinalRule.php
+++ tests/PHPStan/Classes/ConcreteClassNotFinalRule.php
@@ -1,17 +1,18 @@
 <?php
 
-namespace PhpTuf\ComposerStager\Tests\PHPStan;
+namespace PhpTuf\ComposerStager\Tests\PHPStan\Classes;
 
 use PhpParser\Node;
 use PHPStan\Analyser\Scope;
 use PHPStan\Rules\RuleErrorBuilder;
+use PhpTuf\ComposerStager\Tests\PHPStan\Classes\AbstractRule;
 
 /**
- * Requires concrete classes to be final.
+ * Requires concrete non-application classes to be final.
  *
  * @see https://ocramius.github.io/blog/when-to-declare-classes-final/
  */
-class FinalRule extends AbstractRule
+class ConcreteClassNotFinalRule extends AbstractRule
 {
     public function processNode(Node $node, Scope $scope): array
     {
