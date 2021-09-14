d07c06e8555a0312fb4b94f4887b4fcf4143aee7
Travis Carden <travis.carden@gmail.com>
2021-09-14 12:23:17 -0400

    Remove now-obsolete \PhpTuf\ComposerStager\Util\DirectoryUtil::getDescendantRelativeToAncestor().

--- src/Util/DirectoryUtil.php
+++ src/Util/DirectoryUtil.php
@@ -2,8 +2,6 @@
 
 namespace PhpTuf\ComposerStager\Util;
 
-use PhpTuf\ComposerStager\Exception\LogicException;
-
 /**
  * @internal
  */
@@ -40,35 +38,5 @@ final class DirectoryUtil
     public static function ensureTrailingSlash(string $path): string
     {
         return self::stripTrailingSlash($path) . DIRECTORY_SEPARATOR;
-    }
-
-    /**
-     * Gets the path of a given path relative to a given base.
-     *
-     * @param string $ancestor
-     *   A path, absolute or relative, existing or not.
-     * @param string $descendant
-     *   A path, relative to the given ancestor, existing or not.
-     *
-     * @throws \PhpTuf\ComposerStager\Exception\LogicException
-     */
-    public static function getDescendantRelativeToAncestor(string $ancestor, string $descendant): string
-    {
-        if (self::stripTrailingSlash($ancestor) === self::stripTrailingSlash($descendant)) {
-            throw new LogicException('Ancestor cannot be equal to descendant.');
-        }
-
-        $ancestorDirLen = strlen(self::ensureTrailingSlash($ancestor));
-        if ($ancestor === '') {
-            $ancestorDirLen = 0;
-        }
-
-        $relativePath = substr($descendant, $ancestorDirLen);
-
-        if ((bool) $relativePath === false) {
-            throw new LogicException('Descendant is outside ancestor.');
-        }
-
-        return self::stripTrailingSlash($relativePath);
     }
 }
