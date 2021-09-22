033f0e27e6f373c23df6b931e61cc4cbe6be9bfa
Travis Carden <travis.carden@gmail.com>
2021-09-22 15:11:22 -0400

    Set version number back to dev.

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -14,7 +14,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 final class Application extends \Symfony\Component\Console\Application
 {
     private const NAME = 'Composer Stager';
-    private const VERSION = 'v0.2.2';
+    private const VERSION = 'v0.3.0-dev';
 
     public const ACTIVE_DIR_OPTION = 'active-dir';
     public const STAGING_DIR_OPTION = 'staging-dir';
