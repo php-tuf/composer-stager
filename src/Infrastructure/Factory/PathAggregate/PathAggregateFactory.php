3bed548020ff9338f9ae9ce89df0e3685b63d393
Travis Carden <travis.carden@gmail.com>
2022-02-01 11:20:13 -0500

    Refactor Infrastructure\Factory\PathAggregate\PathAggregateFactory::create() slightly for clarity.

--- src/Infrastructure/Factory/PathAggregate/PathAggregateFactory.php
+++ src/Infrastructure/Factory/PathAggregate/PathAggregateFactory.php
@@ -7,7 +7,6 @@ use PhpTuf\ComposerStager\Domain\Value\Path\PathInterface;
 use PhpTuf\ComposerStager\Exception\InvalidArgumentException;
 use PhpTuf\ComposerStager\Infrastructure\Aggregate\PathAggregate\PathAggregate;
 use PhpTuf\ComposerStager\Infrastructure\Factory\Path\PathFactory;
-use TypeError;
 
 /**
  * Creates path aggregates.
@@ -17,7 +16,7 @@ final class PathAggregateFactory
     /**
      * Creates a path aggregate from an array of strings.
      *
-     * @param string[] $paths
+     * @param string[]|mixed[] $paths
      *   An array of path strings, as absolute or relative to the current working
      *   directory as returned by `getcwd()` at runtime, e.g., "/var/www/example"
      *   or "example". Nothing needs to actually exist at the paths.
@@ -27,15 +26,13 @@ final class PathAggregateFactory
     public static function create(array $paths): PathAggregateInterface
     {
         $paths = array_map(static function ($path): PathInterface {
-            try {
-                return PathFactory::create($path);
-            } catch (TypeError $e) { // @phpstan-ignore-line Suppress false positive:
-                // "TypeError is never thrown in the corresponding try block".
+            if (!is_string($path)) {
                 throw new InvalidArgumentException(sprintf(
                     'Paths must be strings. Given %s.',
                     var_export($path, true)
-                ), $e->getCode(), $e);
+                ));
             }
+            return PathFactory::create($path);
         }, $paths);
         return new PathAggregate($paths);
     }
