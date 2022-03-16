c563d75021cc621d64658bea3b408d249aacc011
Travis Carden <travis.carden@gmail.com>
2022-03-15 23:02:30 -0400

    Add Domain\Service\Precondition\PreconditionInterface and Domain\Exception\PreconditionException.

---
+++ src/Domain/Exception/PreconditionException.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+
+namespace PhpTuf\ComposerStager\Domain\Exception;
+
+use PhpTuf\ComposerStager\Domain\Service\Precondition\PreconditionInterface;
+use RuntimeException;
+use Throwable;
+
+class PreconditionException extends RuntimeException implements ExceptionInterface
+{
+    /** @var \PhpTuf\ComposerStager\Domain\Service\Precondition\PreconditionInterface */
+    private $precondition;
+
+    public function __construct(
+        PreconditionInterface $precondition,
+        string $message = '',
+        int $code = 0,
+        ?Throwable $previous = null
+    ) {
+        $this->precondition = $precondition;
+
+        parent::__construct($message, $code, $previous);
+    }
+
+    public function getPrecondition(): PreconditionInterface
+    {
+        return $this->precondition;
+    }
+}
