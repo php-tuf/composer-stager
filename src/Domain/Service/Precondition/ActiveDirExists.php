a7f265f8ea7b56a898494b95fb10df3533665b92
Travis Carden <travis.carden@gmail.com>
2022-03-22 11:54:31 -0400

    Add Domain\Service\Precondition\ActiveDirExists.

---
+++ src/Domain/Service/Precondition/ActiveDirExists.php
@@ -0,0 +1,44 @@
+<?php declare(strict_types=1);
+
+namespace PhpTuf\ComposerStager\Domain\Service\Precondition;
+
+use PhpTuf\ComposerStager\Domain\Service\Filesystem\FilesystemInterface;
+use PhpTuf\ComposerStager\Domain\Value\Path\PathInterface;
+
+final class ActiveDirExists extends AbstractPrecondition implements ActiveDirExistsInterface
+{
+    /** @var \PhpTuf\ComposerStager\Domain\Service\Filesystem\FilesystemInterface */
+    private $filesystem;
+
+    public function __construct(FilesystemInterface $filesystem)
+    {
+        $this->filesystem = $filesystem;
+
+        parent::__construct();
+    }
+
+    public static function getName(): string
+    {
+        return 'Active directory exists'; // @codeCoverageIgnore
+    }
+
+    public static function getDescription(): string
+    {
+        return 'There must be an active directory present before any operations can be performed.'; // @codeCoverageIgnore
+    }
+
+    public function isFulfilled(PathInterface $activeDir, PathInterface $stagingDir): bool
+    {
+        return $this->filesystem->exists($activeDir->resolve());
+    }
+
+    protected function getFulfilledStatusMessage(): string
+    {
+        return 'The active directory exists.'; // @codeCoverageIgnore
+    }
+
+    protected function getUnfulfilledStatusMessage(): string
+    {
+        return 'The active directory does not exist.'; // @codeCoverageIgnore
+    }
+}
---
+++ tests/PHPUnit/Domain/Service/Precondition/ActiveDirExistsUnitTest.php
@@ -0,0 +1,78 @@
+<?php declare(strict_types=1);
+
+namespace PhpTuf\ComposerStager\Tests\PHPUnit\Domain\Service\Precondition;
+
+use PhpTuf\ComposerStager\Domain\Exception\PreconditionException;
+use PhpTuf\ComposerStager\Domain\Service\Filesystem\FilesystemInterface;
+use PhpTuf\ComposerStager\Domain\Service\Precondition\ActiveDirExists;
+use PhpTuf\ComposerStager\Domain\Value\Path\PathInterface;
+use PhpTuf\ComposerStager\Tests\PHPUnit\TestCase;
+
+/**
+ * @coversDefaultClass \PhpTuf\ComposerStager\Domain\Service\Precondition\ActiveDirExists
+ *
+ * @covers \PhpTuf\ComposerStager\Domain\Service\Precondition\ActiveDirExists::__construct
+ *
+ * @uses \PhpTuf\ComposerStager\Domain\Exception\PreconditionException
+ * @uses \PhpTuf\ComposerStager\Domain\Service\Precondition\AbstractPrecondition
+ *
+ * @property \PhpTuf\ComposerStager\Domain\Value\Path\PathInterface|\Prophecy\Prophecy\ObjectProphecy $activeDir
+ * @property \PhpTuf\ComposerStager\Domain\Value\Path\PathInterface|\Prophecy\Prophecy\ObjectProphecy $stagingDir
+ * @property \PhpTuf\ComposerStager\Domain\Service\Filesystem\FilesystemInterface|\Prophecy\Prophecy\ObjectProphecy $filesystem
+ */
+class ActiveDirExistsUnitTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        $this->activeDir = $this->prophesize(PathInterface::class);
+        $this->activeDir
+            ->resolve()
+            ->willReturn(self::ACTIVE_DIR);
+        $this->stagingDir = $this->prophesize(PathInterface::class);
+        $this->stagingDir
+            ->resolve()
+            ->willReturn(self::STAGING_DIR);
+        $this->filesystem = $this->prophesize(FilesystemInterface::class);
+    }
+
+    protected function createSut(): ActiveDirExists
+    {
+        $filesystem = $this->filesystem->reveal();
+        return new ActiveDirExists($filesystem);
+    }
+
+    /** @covers ::isFulfilled */
+    public function testIsFulfilled(): void
+    {
+        $activeDir = $this->activeDir->reveal();
+        $stagingDir = $this->stagingDir->reveal();
+        $this->filesystem
+            ->exists($activeDir->resolve())
+            ->shouldBeCalledOnce()
+            ->willReturn(true);
+        $sut = $this->createSut();
+
+        self::assertEquals(true, $sut->isFulfilled($activeDir, $stagingDir));
+    }
+
+    /**
+     * @covers ::__construct
+     * @covers ::isFulfilled
+     */
+    public function testIsUnfulfilled(): void
+    {
+        $this->expectException(PreconditionException::class);
+
+        $activeDir = $this->activeDir->reveal();
+        $stagingDir = $this->stagingDir->reveal();
+        $this->filesystem
+            ->exists($activeDir->resolve())
+            ->willReturn(false);
+
+        $sut = $this->createSut();
+
+        self::assertFalse($sut->isFulfilled($activeDir, $stagingDir));
+
+        $sut->assertIsFulfilled($activeDir, $stagingDir);
+    }
+}
