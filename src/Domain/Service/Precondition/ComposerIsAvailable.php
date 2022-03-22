ed5f1526e5c0d97c4f049bacdf4a66e8d8d11f1c
Travis Carden <travis.carden@gmail.com>
2022-03-22 00:22:08 -0400

    Add Domain\Service\Precondition\ComposerIsAvailable.

---
+++ src/Domain/Service/Precondition/ComposerIsAvailable.php
@@ -0,0 +1,51 @@
+<?php declare(strict_types=1);
+
+namespace PhpTuf\ComposerStager\Domain\Service\Precondition;
+
+use PhpTuf\ComposerStager\Domain\Exception\IOException;
+use PhpTuf\ComposerStager\Domain\Value\Path\PathInterface;
+use PhpTuf\ComposerStager\Infrastructure\Service\Finder\ExecutableFinderInterface;
+
+final class ComposerIsAvailable extends AbstractPrecondition implements ComposerIsAvailableInterface
+{
+    /** @var \PhpTuf\ComposerStager\Infrastructure\Service\Finder\ExecutableFinderInterface */
+    private $executableFinder;
+
+    public function __construct(ExecutableFinderInterface $executableFinder)
+    {
+        $this->executableFinder = $executableFinder;
+
+        parent::__construct();
+    }
+
+    public static function getName(): string
+    {
+        return 'Composer'; // @codeCoverageIgnore
+    }
+
+    public static function getDescription(): string
+    {
+        return 'Composer must be available in order to stage commands.'; // @codeCoverageIgnore
+    }
+
+    public function isFulfilled(PathInterface $activeDir, PathInterface $stagingDir): bool
+    {
+        try {
+            $this->executableFinder->find('composer');
+        } catch (IOException $e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    protected function getFulfilledStatusMessage(): string
+    {
+        return 'Composer is available.'; // @codeCoverageIgnore
+    }
+
+    protected function getUnfulfilledStatusMessage(): string
+    {
+        return 'Composer cannot be found.'; // @codeCoverageIgnore
+    }
+}
---
+++ tests/PHPUnit/Domain/Service/Precondition/ComposerIsAvailablePreconditionUnitTest.php
@@ -0,0 +1,64 @@
+<?php declare(strict_types=1);
+
+namespace PhpTuf\ComposerStager\Tests\PHPUnit\Domain\Service\Precondition;
+
+use PhpTuf\ComposerStager\Domain\Exception\IOException;
+use PhpTuf\ComposerStager\Domain\Service\Precondition\ComposerIsAvailable;
+use PhpTuf\ComposerStager\Domain\Value\Path\PathInterface;
+use PhpTuf\ComposerStager\Infrastructure\Service\Finder\ExecutableFinderInterface;
+use PhpTuf\ComposerStager\Tests\PHPUnit\TestCase;
+
+/**
+ * @coversDefaultClass \PhpTuf\ComposerStager\Domain\Service\Precondition\ComposerIsAvailable
+ *
+ * @covers \PhpTuf\ComposerStager\Domain\Service\Precondition\ComposerIsAvailable::__construct
+ *
+ * @uses \PhpTuf\ComposerStager\Domain\Service\Precondition\AbstractPrecondition
+ *
+ * @property \PhpTuf\ComposerStager\Domain\Value\Path\PathInterface|\Prophecy\Prophecy\ObjectProphecy $activeDir
+ * @property \PhpTuf\ComposerStager\Domain\Value\Path\PathInterface|\Prophecy\Prophecy\ObjectProphecy $stagingDir
+ * @property \PhpTuf\ComposerStager\Infrastructure\Service\Finder\ExecutableFinderInterface|\Prophecy\Prophecy\ObjectProphecy $executableFinder
+ */
+class ComposerIsAvailablePreconditionUnitTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        $this->activeDir = $this->prophesize(PathInterface::class);
+        $this->stagingDir = $this->prophesize(PathInterface::class);
+        $this->executableFinder = $this->prophesize(ExecutableFinderInterface::class);
+    }
+
+    protected function createSut(): ComposerIsAvailable
+    {
+        $executableFinder = $this->executableFinder->reveal();
+        return new ComposerIsAvailable($executableFinder);
+    }
+
+    /** @covers ::isFulfilled */
+    public function testIsFulfilled(): void
+    {
+        $activeDir = $this->activeDir->reveal();
+        $stagingDir = $this->stagingDir->reveal();
+        $this->executableFinder
+            ->find('composer')
+            ->shouldBeCalledOnce()
+            ->willReturn('/usr/local/bin/composer');
+        $sut = $this->createSut();
+
+        self::assertTrue($sut->isFulfilled($activeDir, $stagingDir));
+    }
+
+    /** @covers ::isFulfilled */
+    public function testIsUnfulfilled(): void
+    {
+        $activeDir = $this->activeDir->reveal();
+        $stagingDir = $this->stagingDir->reveal();
+        $this->executableFinder
+            ->find('composer')
+            ->shouldBeCalledOnce()
+            ->willThrow(IOException::class);
+        $sut = $this->createSut();
+
+        self::assertFalse($sut->isFulfilled($activeDir, $stagingDir));
+    }
+}
