d489a00d55195db2853eee1898bcd0353b57d6be
Travis Carden <travis.carden@gmail.com>
2022-03-22 12:12:07 -0400

    Add Domain\Service\Precondition\ActiveDirIsWritable.

---
+++ src/Domain/Service/Precondition/ActiveDirIsWritableInterface.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+namespace PhpTuf\ComposerStager\Domain\Service\Precondition;
+
+/**
+ * This interface exists solely to facilitate autowiring dependencies through type hinting.
+ *
+ * @see https://symfony.com/doc/current/service_container/autowiring.html
+ */
+interface ActiveDirIsWritableInterface extends PreconditionInterface
+{
+}
