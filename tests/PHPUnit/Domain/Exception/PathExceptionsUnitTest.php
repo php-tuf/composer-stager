a4df72df3406fd5a0893c00800bc19487cb3288b
Travis Carden <travis.carden@gmail.com>
2022-04-13 18:15:57 -0400

    Collapse Domain\Exception\FileNotFoundException into Domain\Exception\PathException.

--- tests/PHPUnit/Domain/Exception/PathExceptionsUnitTest.php
+++ tests/PHPUnit/Domain/Exception/PathExceptionsUnitTest.php
@@ -2,7 +2,6 @@
 
 namespace PhpTuf\ComposerStager\Tests\PHPUnit\Domain\Exception;
 
-use PhpTuf\ComposerStager\Domain\Exception\FileNotFoundException;
 use PhpTuf\ComposerStager\Domain\Exception\PathException;
 use PhpTuf\ComposerStager\Tests\PHPUnit\TestCase;
 
@@ -10,25 +9,14 @@ final class PathExceptionsUnitTest extends TestCase
 {
     /**
      * @covers \PhpTuf\ComposerStager\Domain\Exception\PathException
-     * @covers \PhpTuf\ComposerStager\Domain\Exception\FileNotFoundException
-     * @covers \PhpTuf\ComposerStager\Domain\Exception\PathException
      *
      * @dataProvider providerBasicFunctionality
      */
-    public function testBasicFunctionality(
-        $args,
-        $path,
-        $expectedPathMessage,
-        $expectedDirectoryAlreadyExistsMessage,
-        $expectedDirectoryNotFoundMessage,
-        $expectedDirectoryNotWritableMessage,
-        $expectedFileNotFoundMessage
-    ): void {
+    public function testBasicFunctionality($args, $path, $expectedPathMessage): void
+    {
         $pathException = new PathException(...$args);
-        $fileNotFoundException = new FileNotFoundException(...$args);
 
         self::assertSame($expectedPathMessage, $pathException->getMessage());
-        self::assertSame($expectedFileNotFoundMessage, $fileNotFoundException->getMessage());
     }
 
     public function providerBasicFunctionality(): array
@@ -39,30 +27,20 @@ final class PathExceptionsUnitTest extends TestCase
                 'args' => ['/one'],
                 'path' => '/one',
                 'expectedPathMessage' => '',
-                'expectedDirectoryAlreadyExistsMessage' => 'Directory already exists: "/one"',
                 'expectedDirectoryNotFoundMessage' => 'No such directory: "/one"',
-                'expectedDirectoryNotWritableMessage' => 'Directory not writable: "/one"',
-                'expectedFileNotFoundMessage' => 'No such file: "/one"',
             ],
             // Completely override message.
             [
                 'args' => ['/two', 'Example message'],
                 'path' => '/two',
                 'expectedPathMessage' => 'Example message',
-                'expectedDirectoryAlreadyExistsMessage' => 'Example message',
                 'expectedDirectoryNotFound' => 'Example message',
-                'expectedDirectoryNotWritableMessage' => 'Example message',
-                'expectedFileNotFound' => 'Example message',
             ],
             // Override message with path substitution.
             [
                 'args' => ['/three/four', 'Example message: "%s"'],
                 'path' => '/three/four',
-                'expectedDirectoryAlreadyExistsMessage' => 'Example message: "/three/four"',
                 'expectedPathMessage' => 'Example message: "/three/four"',
-                'expectedDirectoryNotFound' => 'Example message: "/three/four"',
-                'expectedDirectoryNotWritableMessage' => 'Example message: "/three/four"',
-                'expectedFileNotFound' => 'Example message: "/three/four"',
             ],
         ];
     }
