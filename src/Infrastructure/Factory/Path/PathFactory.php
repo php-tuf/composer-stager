9d8b83a4e26468c90245734628a79d417a05a49b
Travis Carden <travis.carden@gmail.com>
2022-05-09 11:37:36 -0400

    Make Infrastructure\Factory\Path\PathFactory injectable and replace direct static accesses.

--- src/Infrastructure/Factory/Path/PathFactory.php
+++ src/Infrastructure/Factory/Path/PathFactory.php
@@ -6,17 +6,8 @@ use PhpTuf\ComposerStager\Domain\Value\Path\PathInterface;
 use PhpTuf\ComposerStager\Infrastructure\Value\Path\UnixLikePath;
 use PhpTuf\ComposerStager\Infrastructure\Value\Path\WindowsPath;
 
-/** Creates path value objects. */
-final class PathFactory
+final class PathFactory implements PathFactoryInterface
 {
-    /**
-     * Creates a path value object from a string.
-     *
-     * @param string $path
-     *   The path string, as absolute or relative to the current working directory
-     *   as returned by `getcwd()` at runtime, e.g., "/var/www/example" or
-     *   "example". Nothing needs to actually exist at the path.
-     */
     public static function create(string $path): PathInterface
     {
         if (DIRECTORY_SEPARATOR === '\\') {
