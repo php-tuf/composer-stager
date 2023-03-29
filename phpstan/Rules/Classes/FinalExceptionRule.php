0e0915201cd1b6fb56bf79b694a3209ad98dd25f
Travis Carden <travis.carden@gmail.com>
2023-03-29 02:06:25 -0400

    Add PHPSstan rule to forbid exceptions from being final.

---
+++ phpstan/Rules/Classes/FinalExceptionRule.php
@@ -0,0 +1,38 @@
+<?php declare(strict_types=1);
+
+namespace PhpTuf\ComposerStager\PHPStan\Rules\Classes;
+
+use PhpParser\Node;
+use PhpParser\Node\Stmt\Class_;
+use PHPStan\Analyser\Scope;
+use PhpTuf\ComposerStager\Domain\Exception\ExceptionInterface;
+use PhpTuf\ComposerStager\PHPStan\Rules\AbstractRule;
+use Throwable;
+
+/** Forbids exceptions from being final. */
+final class FinalExceptionRule extends AbstractRule
+{
+    public function getNodeType(): string
+    {
+        return Class_::class;
+    }
+
+    public function processNode(Node $node, Scope $scope): array
+    {
+        $class = $this->getClassReflection($node);
+
+        if ($class === null) {
+            return [];
+        }
+
+        if (!$class->is(Throwable::class)) {
+            return[];
+        }
+
+        if ($class->isFinal()) {
+            return [$this->buildErrorMessage('Exception cannot be final')];
+        }
+
+        return [];
+    }
+}
