25bd264111b12d0588f426bce5947f041500ef77
Travis Carden <travis.carden@gmail.com>
2023-06-22 19:37:30 -0400

    Introduce API\Translation\Value\Domain.

---
+++ src/API/Translation/Value/Domain.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace PhpTuf\ComposerStager\API\Translation\Value;
+
+/** Provides domain values for the translation system. */
+enum Domain
+{
+    public const DEFAULT = 'messages';
+    public const EXCEPTIONS = 'exceptions';
+}
