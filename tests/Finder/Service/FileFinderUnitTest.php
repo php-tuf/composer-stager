10f4b0dee01cd2be33207e9b6748922272b72ce9
Travis Carden <travis.carden@gmail.com>
2024-02-28 20:52:45 -0500

    Simplify Tests\Finder\Service\FileFinderUnitTest slightly.

--- tests/Finder/Service/FileFinderUnitTest.php
+++ tests/Finder/Service/FileFinderUnitTest.php
@@ -3,13 +3,8 @@
 namespace PhpTuf\ComposerStager\Tests\Finder\Service;
 
 use PhpTuf\ComposerStager\API\Exception\IOException;
-use PhpTuf\ComposerStager\API\Path\Factory\PathFactoryInterface;
-use PhpTuf\ComposerStager\API\Translation\Factory\TranslatableFactoryInterface;
 use PhpTuf\ComposerStager\Internal\Finder\Service\FileFinder;
-use PhpTuf\ComposerStager\Internal\Translation\Factory\TranslatableFactory;
 use PhpTuf\ComposerStager\Tests\TestCase;
-use PhpTuf\ComposerStager\Tests\TestUtils\ContainerTestHelper;
-use Prophecy\Prophecy\ObjectProphecy;
 
 /**
  * @coversDefaultClass \PhpTuf\ComposerStager\Internal\Finder\Service\FileFinder
@@ -18,19 +13,10 @@ use Prophecy\Prophecy\ObjectProphecy;
  */
 final class FileFinderUnitTest extends TestCase
 {
-    private PathFactoryInterface|ObjectProphecy $pathFactory;
-    private TranslatableFactoryInterface $translatableFactory;
-
-    protected function setUp(): void
-    {
-        $this->pathFactory = $this->prophesize(PathFactoryInterface::class);
-        $this->translatableFactory = ContainerTestHelper::get(TranslatableFactory::class);
-    }
-
     private function createSut(): FileFinder
     {
-        $pathFactory = $this->pathFactory->reveal();
-        $translatableFactory = $this->translatableFactory;
+        $pathFactory = self::createPathFactory();
+        $translatableFactory = self::createTranslatableFactory();
         $pathListFactory = self::createPathListFactory();
 
         return new FileFinder($pathFactory, $pathListFactory, $translatableFactory);
